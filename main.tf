a21631032369bcc10adf4cf48129096a9204ab55
ar-36 <ammarzlve@gmail.com>
2024-03-04 18:18:02 +0500

    saasasaasasas

--- main.tf
+++ main.tf
@@ -1,14 +1,10 @@
 
 resource "aws_sns_topic" "ecr_scanner_topic" {
   name = "${var.ENV}-ecr-scanner-topic"
+  kms_master_key_id = aws_kms_key.sns_key.key_id
 }
 
 resource "aws_kms_key" "sns_key" {
   description             = "SNS Topic Encryption Key"
   enable_key_rotation     = true
-}
-
-resource "aws_sns_topic_encryption" "my_topic_encryption" {
-  topic_arn = aws_sns_topic.ecr_scanner_topic.arn
-  key_id    = aws_kms_key.sns_key.arn
 }
